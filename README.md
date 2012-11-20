41cb9d74d985a997da262b2d327609da41cfb939
Abhishek Sabbarwal <abhishek.sabbarwal85@gmail.com>
2012-11-20 00:37:42 -0500

    Committing the Android Hello World Application

---
+++ README.md
@@ -0,0 +1,4 @@
+#This is a sample Hello World Android Application built on Android 4.1
+
+ android:minSdkVersion="8"
+ android:targetSdkVersion="15"
---
+++ src/com/example/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        TextView text = new TextView(this);
+        text.setText("Hello from abhisheksabbarwal.com");
+        setContentView(text);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
